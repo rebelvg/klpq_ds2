47ca62b4a7114a52ebb69450c036c80d44b39e66
Rebel <johnhartigan@ya.ru>
2018-07-28 05:26:33 +0300

    Updated rhs afrf config.

--- klpq_rhsafrf_ds2/ammo/CfgAmmo.hpp
+++ klpq_rhsafrf_ds2/ammo/CfgAmmo.hpp
@@ -1,8 +1,18 @@
 class rhs_ammo_s8;
-class rhs_ammo_ch29;
+class Missile_AGM_02_F;
+class rhs_ammo_kh29l;
 class rhs_ammo_s13b: rhs_ammo_s8 {
     SoundSetExplosion[] = {"DS_Ex_rocket_small_SoundSet","DS_exploSmall_Tail_placeholder_SoundSet"};
 };
-class rhs_ammo_kh55sm: rhs_ammo_ch29 {
+class rhs_ammo_kh29_base: Missile_AGM_02_F {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
+};
+class rhs_ammo_kh55sm: rhs_ammo_kh29l {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
+};
+class rhs_ammo_s24_base: rhs_ammo_s13b {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
+};
+class RHS_9M79_1_F: rhs_ammo_kh29_base {
     SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
 };
