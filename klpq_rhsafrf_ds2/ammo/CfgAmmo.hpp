66332517efacd0ada51be8b20facb8ddc1bfbd56
Rebel <johnhartigan@ya.ru>
2019-08-15 22:48:34 +0300

    Updated rhs ds2.

--- klpq_rhsafrf_ds2/ammo/CfgAmmo.hpp
+++ klpq_rhsafrf_ds2/ammo/CfgAmmo.hpp
@@ -1,13 +1,25 @@
 class rhs_ammo_s8;
+class PipeBombBase;
 class Missile_AGM_02_F;
 class rhs_ammo_kh29l;
+class rhs_ammo_fab250;
+class rhs_ammo_kab250;
 class rhs_ammo_s13b: rhs_ammo_s8 {
     SoundSetExplosion[] = {"DS_Ex_rocket_small_SoundSet","DS_exploSmall_Tail_placeholder_SoundSet"};
+};
+class rhs_ec75_ammo: PipeBombBase {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
 };
 class rhs_ammo_kh29_base: Missile_AGM_02_F {
     SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
 };
 class rhs_ammo_kh55sm: rhs_ammo_kh29l {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
+};
+class rhs_ammo_fab500: rhs_ammo_fab250 {
+    SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
+};
+class rhs_ammo_kab500: rhs_ammo_kab250 {
     SoundSetExplosion[] = {"DS_Ex_heavy_impact_1_SoundSet","DS_exploBig_Tail_placeholder_SoundSet"};
 };
 class rhs_ammo_s24_base: rhs_ammo_s13b {
